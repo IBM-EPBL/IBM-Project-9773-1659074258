18ab17d831cc20596cce9fa1dff69651bd3c8e09
Hiruthik19 <113816038+Hiruthik19@users.noreply.github.com>
2022-10-03 18:59:35 +0530

    Create app.py

---
+++ Assignment/HIRUTHIK BOSE/assignment 1/app.py
@@ -0,0 +1,20 @@
+from flask import render_template,Flask,request
+app=Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("register.html")
+
+@app.route("/Register", methods=["POST","GET"])
+
+def Register():
+     if request.method=="POST":
+         name=request.form.get('name')
+         age=request.form.get('age')
+         mail=request.form.get('mail')
+         qual=request.form.get('qual')
+         return render_template("result.html",name=name,age=age,mail=mail,qual=qual )
+
+if __name__=='__main__':
+    app.run(debug=False)   
