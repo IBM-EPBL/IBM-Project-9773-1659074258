63c9169fd3c520e75a71c2aef50e8833d7d2d170
Charan-2001 <79699471+Charan-2001@users.noreply.github.com>
2022-10-08 17:29:03 +0530

    Update app.py

--- Assignment/Charan/Assignment_1/app.py
+++ Assignment/Charan/Assignment_1/app.py
@@ -1,1 +1,21 @@
+from flask import render_template,Flask,request
 
+app=Flask(__name__)
+@app.route("/")
+
+def index():
+    return render_template("register.html")
+
+@app.route("/Register", methods=["POST","GET"])
+
+def Register():
+     if request.method=="POST":
+         name=request.form.get('name')
+         age=request.form.get('age')
+         mail=request.form.get('mail')
+         qual=request.form.get('qual')
+         phno=request.form.get('phno')
+         return render_template("result.html",name=name,age=age,mail=mail,qual=qual,phno=phno )
+
+if __name__=='__main__':
+    app.run(debug=True) 
