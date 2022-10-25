131763afa218e4ef3271b5b61c75a35a84716b3a
Charan-2001 <79699471+Charan-2001@users.noreply.github.com>
2022-10-25 20:59:34 +0530

    Update DB2_Connect.py

--- Assignment/Charan/Assignment_2/DB2_Connect.py
+++ Assignment/Charan/Assignment_2/DB2_Connect.py
@@ -1,1 +1,13 @@
+from flask import Flask
+import ibm_db
 
+app=Flask(__name__)
+app.secret_key="a"
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=1bbf73c5-d84a-4bb0-85b9-ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=32286;SECURITY=SSL;SSLServerCERTIFICATE=DigiCertGlobalRootCA.crt;UID=ygb78843;PWD=kMybE3BM4ArGXlWQ;",'','')
+
+@app.route('/')
+def home():   
+    return "This File is linked to the DB2 database"
+
+if __name__=="__main__":
+    app.run(debug=True)
